51309069fa6e9eeca41e1044aa15f08f46dfb54c
DK <156811351+dmtkfs@users.noreply.github.com>
2025-05-12 19:52:29 -0400

    push

--- rust/pg_tamperlog_rust/src/lib.rs
+++ rust/pg_tamperlog_rust/src/lib.rs
@@ -1,9 +1,11 @@
 use pgrx::prelude::*;
-pg_module_magic!();
+use sha2::{Sha256, Digest};
+use hex;
+
+pgrx::pg_module_magic!();
 
 #[pg_extern]
 fn sha256_fast(input: &str) -> String {
-    use sha2::{Digest, Sha256};
     let mut hasher = Sha256::new();
     hasher.update(input.as_bytes());
     hex::encode(hasher.finalize())
